59dea03ec992948d8087824763ccd3715434e850
ysKim <znxkznxk1030@gmail.com>
2022-02-14 11:01:17 +0900

    표편집

--- lv3.표편집(2021-카카오인턴쉽)/ps/Problem.java
+++ lv3.표편집(2021-카카오인턴쉽)/ps/Problem.java
@@ -5,7 +5,7 @@ public class Problem {
   public static void main(String[] args) {
     Solution s = new Solution();
 
-    String[] cmds = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
+    String[] cmds = { "D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"};
 
     System.out.println(s.solution(8, 2, cmds));
   }
--- lv3.표편집(2021-카카오인턴쉽)/ps/Solution.java
+++ lv3.표편집(2021-카카오인턴쉽)/ps/Solution.java
@@ -1,116 +1,120 @@
 package ps;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class Solution {
-  List<User> list = new LinkedList<>();
-  Stack<User> trace = new Stack();
-  int cursor = -1;
 
-  public class User {
-    int orgIndex;
-    boolean isDeleted;
+  List<Node> list = new ArrayList<>();
+  Stack<Node> trace = new Stack<>();
+  Node curNode = null;
+  
+  public class Node {
+    int index;
+    Node prev = null;
+    Node next = null;
+    
+    public Node(int index) {
+      this.index = index;
+    }
 
-    public User(int orgIndex, boolean isDeleted) {
-      this.orgIndex = orgIndex;
-      this.isDeleted = isDeleted;
+    public Node(int index, Node prev, Node next) {
+      this.index = index;
+      this.prev = prev;
+      this.next = next;
     }
   }
 
-  private void selectUp(int u) {
-    for (int i = 0; i < u;) {
-      User user = list.get(--cursor);
-      if (!user.isDeleted) {
-        i++;
-      }
+  private void initializeList(int n) {
+    for (int i = 0; i < n; i++) {
+      list.add(new Node(i));
+    }
+
+    list.get(0).next = list.get(1);
+    list.get(list.size() - 1).prev = list.get(list.size() - 2);
+
+    for (int i = 1; i < n - 1; i++) {
+      list.get(i).prev = list.get(i - 1);
+      list.get(i).next = list.get(i + 1);
     }
   }
 
-  private void selectDown(int d) {
-    for (int i = 0; i < d;) {
-      User user = list.get(++cursor);
-      if (!user.isDeleted) {
-        i++;
-      }
+  private void selectUp(int n) {
+    while(n-- > 0){
+      curNode = curNode.prev;
+    }
+  }
+
+  private void selectDown(int n) {
+    while (n-- > 0) {
+      curNode = curNode.next;
     }
   }
 
   private void delete() {
-    User user = list.get(cursor);
-    user.isDeleted = true;
-    trace.add(user);
-
-    boolean isMove = false;
-    int origin = cursor;
-
-    while (true) {
-      if (cursor + 1 >= list.size()) break;
-      User nextUser = list.get(++cursor);
-      if (!nextUser.isDeleted) {
-        isMove = true;
-        break;
-      }
-    }
-
-    if (!isMove) {
-      cursor = origin;
-      while (true) {
-        User nextUser = list.get(--cursor);
-        if (!nextUser.isDeleted) {
-          isMove = true;
-          break;
-        }
-      }
+    trace.push(curNode);
+    Node prevNode = curNode.prev;
+    Node nextNode = curNode.next;
+    if (nextNode != null && prevNode != null) {
+      prevNode.next = nextNode;
+      nextNode.prev = prevNode;
+      curNode = nextNode;
+    } else if (nextNode == null && prevNode != null) {
+      prevNode.next = nextNode;
+      curNode = prevNode;
+    } else if (nextNode != null && prevNode == null) {
+      nextNode.prev = prevNode;
+      curNode = nextNode;
     }
   }
 
   private void undo() {
-    User user = trace.pop();
-    user.isDeleted = false;
+    Node undoNode = trace.pop();
+
+    Node prevNode = undoNode.prev;
+    Node nextNode = undoNode.next;
+
+    if (prevNode != null) prevNode.next = undoNode;
+    if (nextNode != null) nextNode.prev = undoNode;
   }
 
   public String solution(int n, int k, String[] cmds) {
-    cursor = k;
+    initializeList(n);
 
-    for (int i = 0; i < n; i++) {
-      list.add(new User(i, false));
-    }
+    curNode = list.get(k);
 
-    for (String cmd : cmds) {
-      String[] keyword = cmd.split(" ");
-      String code = keyword[0];
-      Integer digit = -1;
-
-      switch (code) {
+    for (String cmd: cmds) {
+      String[] keywords = cmd.split(" ");
+      int num = -1;
+      
+      switch (keywords[0]) {
         case "U":
-          digit = Integer.parseInt(keyword[1]);
-          selectUp(digit);
-          break;
+        num = Integer.parseInt(keywords[1]);
+        selectUp(num);
+        break;
         case "D":
-          digit = Integer.parseInt(keyword[1]);
-          selectDown(digit);
-          break;
+        num = Integer.parseInt(keywords[1]);
+        selectDown(num);
+        break;
         case "C":
-          delete();
-          break;
+        delete();
+        break;
         case "Z":
-          undo();
-          break;
+        undo();
+        break;
       }
     }
 
-    String answer = "";
 
-    for (User user: list) {
-      if (user.isDeleted) {
-        answer = answer + "X";
-      } else {
-        answer = answer + "O";
-      }
+    StringBuilder answer = new StringBuilder("O".repeat(n));
+
+    while(!trace.empty()) {
+      Node node = trace.pop();
+      answer.setCharAt(node.index, 'X');
     }
 
-    return answer;
+    return answer.toString();
   }
 }
